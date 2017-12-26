7fa8e710e693fe94c3148e6078b21c7a86fe4ce1
Neucrack <CZD666666@gmail.com>
2017-12-26 15:23:28 +0800

    update evevnt id

--- include/api_inc/api_event.h
+++ include/api_inc/api_event.h
@@ -6,7 +6,10 @@
 #include <cs_types.h>
 
 typedef enum{
-    API_EVENT_ID_POWER_ON = 0                      ,   //param1: shows power on cause: Power_On_Cause_t
+    //system
+    API_EVENT_ID_POWER_ON = 0                      ,   //param1: shows power on cause:
+    API_EVENT_ID_SYSTEM_READY                      ,
+
     //SIM card
     API_EVENT_ID_NO_SIMCARD                        ,   //param1: card id
     API_EVENT_ID_SIMCARD_DROP                      ,   //param1: card id
@@ -36,14 +39,25 @@ typedef enum{
     API_EVENT_ID_SOCKET_CLOSED                     ,   //param1: socketFd
     API_EVENT_ID_SOCKET_SENT                       ,   //param1: socketFd
     API_EVENT_ID_SOCKET_RECEIVED                   ,   //param1: socketFd, param2: length of data received
-    API_EVENT_ID_SOCKET_ERROR                      ,   //param1: socketFd
-    API_EVENT_ID_SMS_SENT                          ,  
+    API_EVENT_ID_SOCKET_ERROR                      ,   //param1: socketFd, param2: error cause(API_Socket_Error_t)
+
+    //SMS
+    API_EVENT_ID_SMS_SENT                          ,
+    API_EVENT_ID_SMS_RECEIVED                      ,  //param1:SMS_Encode_Type_t, param2:message content length, pParam1:message header info, pParam2:message content
+    API_EVENT_ID_SMS_ERROR                         ,  //param1:SMS_Error_t cause
 
     //uart
     API_EVENT_ID_UART_RECEIVED                     ,   //param1: uart number, param2: length, pParam1: data
 
     //GPS
-    API_EVENT_ID_GPS_UART_RECEIVED                 ,   //pram1: length, pParam1: data
+    API_EVENT_ID_GPS_UART_RECEIVED                 ,   //param1: length, pParam1: data
+
+    //CALL
+    API_EVENT_ID_CALL_DIAL                         ,   //param1: isSuccess, param2:error code(CALL_Error_t)
+    API_EVENT_ID_CALL_HANGUP                       ,   //param1: is remote release call, param2:error code(CALL_Error_t)
+    API_EVENT_ID_CALL_INCOMING                     ,   //param1: number type, pParam1:number
+    API_EVENT_ID_CALL_ANSWER                       ,  
+    API_EVENT_ID_CALL_DTMF                         ,  //param1: key
     
     //error
     API_EVENT_ID_MALLOC_FAILED                     ,
@@ -55,6 +69,7 @@ typedef struct {
     uint32_t       param1 ;
     uint32_t       param2 ;
     uint8_t*       pParam1;
+    uint8_t*       pParam2;
 }API_Event_t;
 
 
