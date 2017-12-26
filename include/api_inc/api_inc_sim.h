3fb47e584a31a9216b5ff9204f7cbb486c8ac7e1
Neucrack <CZD666666@gmail.com>
2017-12-26 15:46:36 +0800

    update sim

---
+++ include/api_inc/api_inc_sim.h
@@ -0,0 +1,12 @@
+#ifndef _API_INC_SIM_H
+#define _API_INC_SIM_H
+
+
+typedef enum{
+    SIM0      = 0,
+    SIM_ID_MAX
+}SIM_ID_t;
+
+
+
+#endif
--- include/api_sim.h
+++ include/api_sim.h
@@ -2,10 +2,16 @@
 #define __API_SIM_H_
 
 
-typedef enum{
-    SIM0      = 0,
-    SIM_ID_MAX
-}SIM_ID_t;
+#include <sdk_init.h>
+
+
+
+/**
+ * 
+ * @param ccid: ccid out, length >= 20
+ * 
+ */
+#define SIM_GetICCID     CSDK_FUNC(SIM_GetICCID)
 
 
 #endif
