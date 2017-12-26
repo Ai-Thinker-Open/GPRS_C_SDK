af16b0c81fe9387175e886307b7757302e4eb800
Neucrack <CZD666666@gmail.com>
2017-12-26 15:15:29 +0800

    add audio

---
+++ include/api_audio.h
@@ -0,0 +1,15 @@
+#ifndef __API_AUDIO_H
+#define __API_AUDIO_H
+
+#include "sdk_init.h"
+
+
+#define AUDIO_MicOpen          CSDK_FUNC(AUDIO_MicOpen)
+#define AUDIO_MicClose         CSDK_FUNC(AUDIO_MicClose)
+#define AUDIO_MicSetMute       CSDK_FUNC(AUDIO_MicSetMute)
+#define AUDIO_SpeakerOpen      CSDK_FUNC(AUDIO_SpeakerOpen)
+#define AUDIO_SpeakerClose     CSDK_FUNC(AUDIO_SpeakerClose)
+#define AUDIO_SpeakerSetVolume CSDK_FUNC(AUDIO_SpeakerSetVolume)
+
+
+#endif
