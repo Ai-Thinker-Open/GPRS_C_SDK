fc99da3f089bd01cf22c1e70de21fa319acca83b
Neucrack <CZD666666@gmail.com>
2017-12-26 15:29:09 +0800

    update charset

--- include/api_charset.h
+++ include/api_charset.h
@@ -8,6 +8,7 @@
 #define GetCharset              CSDK_FUNC(GetCharset)
 #define Unicode2LocalLanguage   CSDK_FUNC(Unicode2LocalLanguage)
 #define LocalLanguage2Unicode   CSDK_FUNC(LocalLanguage2Unicode)
+#define Ascii2Unicode           CSDK_FUNC(Ascii2Unicode)
 
 #endif
 
