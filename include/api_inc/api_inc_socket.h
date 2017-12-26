03f583d9fe6262d10ddf01e9e7e40eb7b53fad14
Neucrack <CZD666666@gmail.com>
2017-12-26 15:52:42 +0800

    update socket

--- include/api_inc/api_inc_socket.h
+++ include/api_inc/api_inc_socket.h
@@ -19,6 +19,45 @@ typedef enum{
     DNS_STATUS_MAX
 }DNS_Status_t;
 
+
+typedef enum{
+    /** No error, everything OK. */
+    API_SOCKET_ERROR_NONE = 0,
+    /** Out of memory error.     */
+    API_SOCKET_ERROR_MEM        = -1,
+    /** Buffer error.            */
+    API_SOCKET_ERROR_BUF        = -2,
+    /** Timeout.                 */
+    API_SOCKET_ERROR_TIMEOUT    = -3,
+    /** Routing problem.         */
+    API_SOCKET_ERROR_RTE        = -4,
+    /** Operation in progress    */
+    API_SOCKET_ERROR_INPROGRESS = -5,
+    /** Illegal value.           */
+    API_SOCKET_ERROR_VAL        = -6,
+    /** Operation would block.   */
+    API_SOCKET_ERROR_WOULDBLOCK = -7,
+    /** Address in use.          */
+    API_SOCKET_ERROR_USE        = -8,
+    /** Already connecting.      */
+    API_SOCKET_ERROR_ALREADY    = -9,
+    /** Conn already established.*/
+    API_SOCKET_ERROR_ISCONN     = -10,
+    /** Not connected.           */
+    API_SOCKET_ERROR_CONN       = -11,
+    /** Low-level netif error    */
+    API_SOCKET_ERROR_IF         = -12,
+
+    /** Connection aborted.      */
+    API_SOCKET_ERROR_ABRT       = -13,
+    /** Connection reset.        */
+    API_SOCKET_ERROR_RST        = -14,
+    /** Connection closed.       */
+    API_SOCKET_ERROR_CLSD       = -15,
+    /** Illegal argument.        */
+    API_SOCKET_ERROR_ARG        = -16
+}API_Socket_Error_t;
+
 typedef VOID (*DNS_CALLBACK_FUNC_T)(void *param);
 
 #endif
