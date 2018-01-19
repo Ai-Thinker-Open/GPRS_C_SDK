81c82fe2a87f3f822b45ce6ae8d1b3b450d533a9
Neucrack <CZD666666@gmail.com>
2018-01-19 10:38:05 +0800

    add mqtt ssl demo

---
+++ demo/mqtt_ssl/include/demo_mqtt.h
@@ -0,0 +1,17 @@
+#ifndef __DEMO_MQTT_H_
+#define __DEMO_MQTT_H_
+
+
+#define BROKER_IP  "mqtt.neucrack.com"
+#define BROKER_PORT 8883
+#define CLIENT_ID  "Neucrack1"
+#define CLIENT_USER NULL
+#define CLIENT_PASS NULL
+#define SUBSCRIBE_TOPIC "$neucrack/app"
+#define PUBLISH_TOPIC   "$neucrack/gprs"
+#define PUBLISH_INTERVAL 10000 //10s
+#define PUBLISH_PAYLOEAD "hello I'm from gprs module"
+
+
+#endif
+
