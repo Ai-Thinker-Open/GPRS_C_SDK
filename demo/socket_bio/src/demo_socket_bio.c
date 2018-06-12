84cc497983a3cf5cbd7ee2381b6091f2ec442145
Neucrack <CZD666666@gmail.com>
2018-06-12 17:59:46 +0800

    add socket bio demo

---
+++ demo/socket_bio/src/demo_socket_bio.c
@@ -0,0 +1,264 @@
+
+
+#include <string.h>
+#include <stdio.h>
+#include <api_os.h>
+#include <api_event.h>
+#include <api_socket.h>
+#include <api_network.h>
+#include <api_debug.h>
+// #include <sockets.h>
+
+/*******************************************************************/
+/////////////////////////socket configuration////////////////////////
+// (online tcp debug tool: http://tt.ai-thinker.com:8000/ttcloud)
+#define SERVER_IP   "www.example.com"
+#define SERVER_PORT 80
+#define SERVER_PATH "/"
+/*******************************************************************/
+
+
+#define MAIN_TASK_STACK_SIZE    (2048 * 2)
+#define MAIN_TASK_PRIORITY      0
+#define MAIN_TASK_NAME          "Socket Test Task"
+
+#define TEST_TASK_STACK_SIZE    (2048 * 2)
+#define TEST_TASK_PRIORITY      0
+#define TEST_TASK_NAME          "Test Task"
+
+static HANDLE socketTaskHandle = NULL;
+static HANDLE testTaskHandle = NULL;
+static HANDLE semStart = NULL;
+
+
+void EventDispatch(API_Event_t* pEvent)
+{
+    switch(pEvent->id)
+    {
+        case API_EVENT_ID_NO_SIMCARD:
+            Trace(10,"!!NO SIM CARD%d!!!!",pEvent->param1);
+            break;
+
+        case API_EVENT_ID_NETWORK_REGISTER_SEARCHING:
+            Trace(2,"network register searching");
+            break;
+        
+        case API_EVENT_ID_NETWORK_REGISTER_DENIED:
+            Trace(2,"network register denied");
+            break;
+        
+        case API_EVENT_ID_NETWORK_REGISTER_NO:
+            Trace(2,"network register no");
+            break;
+        
+        case API_EVENT_ID_NETWORK_REGISTERED_HOME:
+        case API_EVENT_ID_NETWORK_REGISTERED_ROAMING:
+        {
+            uint8_t status;
+            Trace(2,"network register success");
+            bool ret = Network_GetAttachStatus(&status);
+            if(!ret)
+                Trace(1,"get attach staus fail");
+            Trace(1,"attach status:%d",status);
+            if(status == 0)
+            {
+                ret = Network_StartAttach();
+                if(!ret)
+                {
+                    Trace(1,"network attach fail");
+                }
+            }
+            else
+            {
+                Network_PDP_Context_t context = {
+                    .apn        ="cmnet",
+                    .userName   = ""    ,
+                    .userPasswd = ""
+                };
+                Network_StartActive(context);
+            }
+            break;
+        }
+        case API_EVENT_ID_NETWORK_ATTACHED:
+            Trace(2,"network attach success");
+            Network_PDP_Context_t context = {
+                .apn        ="cmnet",
+                .userName   = ""    ,
+                .userPasswd = ""
+            };
+            Network_StartActive(context);
+            break;
+
+        case API_EVENT_ID_NETWORK_ACTIVATED:
+            Trace(2,"network activate success");
+            OS_ReleaseSemaphore(semStart);
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+//http get with no header
+bool Http_Get(const char* domain, int port,const char* path, char* retBuffer, int bufferLen)
+{
+    uint8_t ip[16];
+    //connect server
+    memset(ip,0,sizeof(ip));
+    if(DNS_GetHostByName2(domain,ip) != 0)
+    {
+        Trace(1,"get ip error");
+        return false;
+    }
+    Trace(1,"get ip success:%s -> %s",domain,ip);
+    char* servInetAddr = ip;
+    snprintf(retBuffer,bufferLen,"GET %s HTTP/1.1\r\nHost: %s\r\n\r\n",path,domain);
+    char* pData = retBuffer;
+    int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(fd < 0){
+        Trace(1,"socket fail");
+        return false;
+    }
+    Trace(1,"fd:%d",fd);
+    // ioctl(fd, FIONBIO, &NBIO);
+    // uint8_t cid = Network_GetPdpContextId();
+    // uint8_t myIpLen = 0;
+    // uint8_t simID   = 0;
+    // uint8_t myIp[18]={0};
+    // CFW_GprsGetPdpAddr(cid,&myIpLen,myIp,simID);
+
+    // //bind local ip with socket
+    // addr.sin_len    = 0;
+    // addr.sin_family = CFW_TCPIP_AF_INET;
+    // addr.sin_port   = 0;
+    // addr.sin_addr.s_addr = htonl(myIp[0] << 24 | myIp[1] << 16 | myIp[2] << 8 | myIp[3]);
+    // int ret = bind(fd, (struct sockaddr *)pName, nNameLen);
+
+    struct sockaddr_in sockaddr;
+    memset(&sockaddr,0,sizeof(sockaddr));
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET,servInetAddr,&sockaddr.sin_addr);
+
+    int ret = connect(fd, (struct sockaddr*)&sockaddr, sizeof(struct sockaddr_in));
+    if(ret < 0){
+        Trace(1,"socket connect fail");
+        return false;
+    }
+    Trace(1,"socket connect success");
+    Trace(1,"send request:%s",pData);
+    ret = send(fd, pData, strlen(pData), 0);
+    if(ret < 0){
+        Trace(1,"socket send fail");
+        return false;
+    }
+    Trace(1,"socket send success");
+
+    struct fd_set fds;
+    struct timeval timeout={12,0};
+    FD_ZERO(&fds);
+    FD_SET(fd,&fds);
+    ret = select(fd+1,&fds,NULL,NULL,&timeout);
+    switch(ret)
+    {
+        case -1:
+            Trace(1,"select error");
+            break;
+        case 0:
+            Trace(1,"select timeout");
+        default:
+            if(FD_ISSET(fd,&fds))
+            {
+                Trace(1,"select return:%d",ret);
+                memset(retBuffer,0,bufferLen);
+                ret = recv(fd,retBuffer,bufferLen,0);
+                if(ret < 0)
+                {
+                    Trace(1,"recv error");
+                    return false;
+                }
+                else if(ret == 0)
+                {
+                    Trace(1,"ret == 0");
+                    return false;
+                }
+                Trace(1,"recv len:%d,data:%s",ret,retBuffer);
+
+            }
+            break;
+    }
+    return true;
+}
+
+void Socket_BIO_Test()
+{
+    char buffer[2048];
+    //wait for gprs network connection ok
+    semStart = OS_CreateSemaphore(0);
+    OS_WaitForSemaphore(semStart,OS_TIME_OUT_WAIT_FOREVER);
+    OS_DeleteSemaphore(semStart);
+
+    //perform http get
+    if(!Http_Get(SERVER_IP,SERVER_PORT,SERVER_PATH,buffer,sizeof(buffer)))
+    {
+        Trace(1,"http get fail");
+    }
+    else
+    {
+        //show response message though tracer(pay attention:tracer can not show all the word if the body too long)
+        Trace(1,"http get success,ret:%s",buffer);
+        char* index0 = strstr(buffer,"\r\n\r\n");
+        char temp = index0[4];
+        index0[4] = '\0';
+        Trace(1,"http response header:%s",buffer);
+        index0[4] = temp;
+        Trace(1,"http response body:%s",index0+4);
+    }
+}
+
+void test_MainTask(void* param)
+{
+    API_Event_t* event=NULL;
+
+    Socket_BIO_Test();
+    
+    while(1)
+    {
+        if(OS_WaitEvent(socketTaskHandle, (void**)&event, OS_TIME_OUT_WAIT_FOREVER))
+        {
+            // EventDispatch(event);
+            OS_Free(event->pParam1);
+            OS_Free(event->pParam2);
+            OS_Free(event);
+        }
+    }
+}
+
+
+void socket_MainTask(void *pData)
+{
+    API_Event_t* event=NULL;
+
+    testTaskHandle = OS_CreateTask(test_MainTask,
+        NULL, NULL, TEST_TASK_STACK_SIZE, TEST_TASK_PRIORITY, 0, 0, TEST_TASK_NAME);
+
+    while(1)
+    {
+        if(OS_WaitEvent(socketTaskHandle, (void**)&event, OS_TIME_OUT_WAIT_FOREVER))
+        {
+            EventDispatch(event);
+            OS_Free(event->pParam1);
+            OS_Free(event->pParam2);
+            OS_Free(event);
+        }
+    }
+}
+
+void socket_bio_Main()
+{
+    socketTaskHandle = OS_CreateTask(socket_MainTask,
+        NULL, NULL, MAIN_TASK_STACK_SIZE, MAIN_TASK_PRIORITY, 0, 0, MAIN_TASK_NAME);
+    OS_SetUserMainHandle(&socketTaskHandle);
+}
+
