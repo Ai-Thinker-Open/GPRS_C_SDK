dc783731be2ad3a2826eae5808d28a71f061f993
Neucrack <CZD666666@gmail.com>
2018-07-27 18:22:22 +0800

    add attention for mqtt api

--- include/api_mqtt.h
+++ include/api_mqtt.h
@@ -20,13 +20,41 @@ extern "C" {
 
 
 #define MQTT_ClientNew        CSDK_FUNC(MQTT_ClientNew)
+
 #define MQTT_ClientDestroy    CSDK_FUNC(MQTT_ClientDestroy)
+
+/**
+ * 
+ * @attention DO NOT call in callback function, but be called in thread(task)
+ */
 #define MQTT_Connect          CSDK_FUNC(MQTT_Connect)
+
+/**
+ * 
+ * @attention DO NOT call in callback function, but be called in thread(task)
+ */
 #define MQTT_Disconnect       CSDK_FUNC(MQTT_Disconnect)
+
 #define MQTT_SetInPubCallback CSDK_FUNC(MQTT_SetInPubCallback)
+
+/**
+ * 
+ * @attention DO NOT call in callback function, but be called in thread(task)
+ */
 #define MQTT_Subscribe        CSDK_FUNC(MQTT_Subscribe)
+
+/**
+ * 
+ * @attention DO NOT call in callback function, but be called in thread(task)
+ */
 #define MQTT_Unsubscribe      CSDK_FUNC(MQTT_Unsubscribe)
+
+/**
+ * 
+ * @attention DO NOT call in callback function, but be called in thread(task)
+ */
 #define MQTT_Publish          CSDK_FUNC(MQTT_Publish)
+
 #define MQTT_IsConnected      CSDK_FUNC(MQTT_IsConnected)
 
 #ifdef __cplusplus
