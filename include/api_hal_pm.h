468445550792b2fe44255a51d84d793e93c584a6
Neucrack <CZD666666@gmail.com>
2018-09-10 15:14:48 +0800

    update api name

--- include/api_hal_pm.h
+++ include/api_hal_pm.h
@@ -38,6 +38,7 @@ typedef enum{
 #define PM_Voltage          CSDK_FUNC(PM_Voltage)
 #define PM_ShutDown         CSDK_FUNC(PM_ShutDown)
 #define PM_Restart          CSDK_FUNC(PM_Restart)
+#define PM_Reboot           PM_Restart
 #define PowerEnable         CSDK_FUNC(PM_PowerEnable)
 
 // void PM_SetSysMinFreq(PM_Sys_Freq_t freq);
