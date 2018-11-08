6ec51d4a2fccd0e84feaef6d1ffe8623003aa742
Neucrack <CZD666666@gmail.com>
2018-11-08 16:47:15 +0800

    optimiz network auto connect demo

--- demo/mqtt/src/demo_mqtt.c
+++ demo/mqtt/src/demo_mqtt.c
@@ -13,6 +13,9 @@
 #include "demo_mqtt.h"
 
 
+#define PDP_CONTEXT_APN       "cmnet"
+#define PDP_CONTEXT_USERNAME  ""
+#define PDP_CONTEXT_PASSWD    ""
 
 #define MAIN_TASK_STACK_SIZE    (2048 * 2)
 #define MAIN_TASK_PRIORITY      0
@@ -30,7 +33,7 @@ static HANDLE mainTaskHandle = NULL;
 static HANDLE secondTaskHandle = NULL;
 
 static HANDLE semMqttStart = NULL;
-
+MQTT_Connect_Info_t ci;
 
 typedef enum{
     MQTT_EVENT_CONNECTED = 0,
@@ -52,79 +55,135 @@ typedef enum{
 MQTT_Status_t mqttStatus = MQTT_STATUS_DISCONNECTED;
 
 
-static void EventDispatch(API_Event_t* pEvent)
+bool AttachActivate()
+{
+    uint8_t status;
+    bool ret = Network_GetAttachStatus(&status);
+    if(!ret)
+    {
+        Trace(2,"get attach staus fail");
+        return false;
+    }
+    Trace(2,"attach status:%d",status);
+    if(!status)
+    {
+        ret = Network_StartAttach();
+        if(!ret)
+        {
+            Trace(2,"network attach fail");
+            return false;
+        }
+    }
+    else
+    {
+        ret = Network_GetActiveStatus(&status);
+        if(!ret)
+        {
+            Trace(2,"get activate staus fail");
+            return false;
+        }
+        Trace(2,"activate status:%d",status);
+        if(!status)
+        {
+            Network_PDP_Context_t context = {
+                .apn        = PDP_CONTEXT_APN,
+                .userName   = PDP_CONTEXT_USERNAME,
+                .userPasswd = PDP_CONTEXT_PASSWD
+            };
+            Network_StartActive(context);
+        }
+    }
+    return true;
+}
+
+static void NetworkEventDispatch(API_Event_t* pEvent)
 {
     switch(pEvent->id)
     {
-        case API_EVENT_ID_NO_SIMCARD:
-            Trace(1,"!!NO SIM CARD%d!!!!",pEvent->param1);
+        case API_EVENT_ID_NETWORK_REGISTER_DENIED:
+            Trace(2,"network register denied");
             break;
 
-        case API_EVENT_ID_SYSTEM_READY:
-            Trace(1,"system initialize complete");
+        case API_EVENT_ID_NETWORK_REGISTER_NO:
+            Trace(2,"network register no");
             break;
-
+        
         case API_EVENT_ID_NETWORK_REGISTERED_HOME:
         case API_EVENT_ID_NETWORK_REGISTERED_ROAMING:
-        {
-            uint8_t status;
             Trace(2,"network register success");
-            bool ret = Network_GetAttachStatus(&status);
-            if(!ret)
-                Trace(1,"get attach staus fail");
-            Trace(1,"attach status:%d",status);
-            if(status == 0)
-            {
-                ret = Network_StartAttach();
-                if(!ret)
-                {
-                    Trace(1,"network attach fail");
-                }
-            }
-            else
-            {
-                Network_PDP_Context_t context = {
-                    .apn        ="cmnet",
-                    .userName   = ""    ,
-                    .userPasswd = ""
-                };
-                Network_StartActive(context);
-            }
-
+            AttachActivate();
             break;
-        }
+
+        case API_EVENT_ID_NETWORK_DETACHED:
+            Trace(2,"network detached");
+            AttachActivate();
+            break;
+        case API_EVENT_ID_NETWORK_ATTACH_FAILED:
+            Trace(2,"network attach failed");
+            AttachActivate();
+            break;
+
         case API_EVENT_ID_NETWORK_ATTACHED:
-            Trace(1,"network attach success");
-            Network_PDP_Context_t context = {
-                .apn        ="cmnet",
-                .userName   = ""    ,
-                .userPasswd = ""
-            };
-            Network_StartActive(context);
+            Trace(2,"network attach success");
+            AttachActivate();
+            break;
+
+        case API_EVENT_ID_NETWORK_DEACTIVED:
+            Trace(2,"network deactived");
+            AttachActivate();
+            break;
+
+        case API_EVENT_ID_NETWORK_ACTIVATE_FAILED:
+            Trace(2,"network activate failed");
+            AttachActivate();
             break;
 
         case API_EVENT_ID_NETWORK_ACTIVATED:
-            Trace(1,"network activate success.."); 
-            OS_ReleaseSemaphore(semMqttStart);
-            break;
-        
-        case API_EVENT_ID_SOCKET_CONNECTED:
-            Trace(1,"socket connected");
-            break;
-        
-        case API_EVENT_ID_SOCKET_CLOSED:
-            Trace(1,"socket closed");
-                      
+            Trace(2,"network activate success..");
             break;
 
         case API_EVENT_ID_SIGNAL_QUALITY:
-            Trace(1,"CSQ:%d",pEvent->param1);
+            Trace(2,"CSQ:%d",pEvent->param1);
             break;
 
         default:
             break;
     }
 }
+
+
+static void EventDispatch(API_Event_t* pEvent)
+{
+    switch(pEvent->id)
+    {    
+        case API_EVENT_ID_NO_SIMCARD:
+            Trace(2,"!!NO SIM CARD%d!!!!",pEvent->param1);
+            break;
+        case API_EVENT_ID_SIMCARD_DROP:
+            Trace(2,"!!SIM CARD%d DROP!!!!",pEvent->param1);
+            break;
+        case API_EVENT_ID_SYSTEM_READY:
+            Trace(2,"system initialize complete");
+            break;
+        case API_EVENT_ID_NETWORK_REGISTER_DENIED:
+        case API_EVENT_ID_NETWORK_REGISTER_NO:
+        case API_EVENT_ID_NETWORK_REGISTERED_HOME:
+        case API_EVENT_ID_NETWORK_REGISTERED_ROAMING:
+        case API_EVENT_ID_NETWORK_DETACHED:
+        case API_EVENT_ID_NETWORK_ATTACH_FAILED:
+        case API_EVENT_ID_NETWORK_ATTACHED:
+        case API_EVENT_ID_NETWORK_DEACTIVED:
+        case API_EVENT_ID_NETWORK_ACTIVATE_FAILED:
+        case API_EVENT_ID_NETWORK_ACTIVATED:
+        case API_EVENT_ID_SIGNAL_QUALITY:
+            NetworkEventDispatch(pEvent);
+            break;
+
+        default:
+            break;
+    }
+}
+
 
 void OnMqttReceived(void* arg, const char* topic, uint32_t payloadLen)
 {
@@ -173,7 +232,9 @@ void OnMqttConnection(MQTT_Client_t *client, void *arg, MQTT_Connection_Status_t
     Trace(1,"MQTT OnMqttConnection() end");
 }
 
+static uint32_t reconnectInterval = 3000;
 void StartTimerPublish(uint32_t interval,MQTT_Client_t* client);
+void StartTimerConnect(uint32_t interval,MQTT_Client_t* client);
 void OnPublish(void* arg, MQTT_Error_t err)
 {
     if(err == MQTT_ERROR_NONE)
@@ -186,6 +247,8 @@ void OnTimerPublish(void* param)
 {
     MQTT_Error_t err;
     MQTT_Client_t* client = (MQTT_Client_t*)param;
+    uint8_t status = MQTT_IsConnected(client);
+    Trace(1,"mqtt status:%d",status);
     if(mqttStatus != MQTT_STATUS_CONNECTED)
     {
         Trace(1,"MQTT not connected to broker! can not publish");
@@ -203,11 +266,40 @@ void StartTimerPublish(uint32_t interval,MQTT_Client_t* client)
     OS_StartCallbackTimer(mainTaskHandle,interval,OnTimerPublish,(void*)client);
 }
 
+void OnTimerStartConnect(void* param)
+{
+    MQTT_Error_t err;
+    MQTT_Client_t* client = (MQTT_Client_t*)param;
+    uint8_t status = MQTT_IsConnected(client);
+    Trace(1,"mqtt status:%d",status);
+    if(mqttStatus == MQTT_STATUS_CONNECTED)
+    {
+        Trace(1,"already connected!");
+        return ;
+    }
+    err = MQTT_Connect(client,BROKER_IP,BROKER_PORT,OnMqttConnection,NULL,&ci);
+    if(err != MQTT_ERROR_NONE)
+    {
+        Trace(1,"MQTT connect fail,error code:%d",err);
+        reconnectInterval += 1000;
+        if(reconnectInterval >= 60000)
+            reconnectInterval = 60000;
+        StartTimerConnect(reconnectInterval,client);
+    }
+}
+
+void StartTimerConnect(uint32_t interval,MQTT_Client_t* client)
+{
+    OS_StartCallbackTimer(mainTaskHandle,interval,OnTimerStartConnect,(void*)client);
+}
+
+
 void SecondTaskEventDispatch(MQTT_Event_t* pEvent)
 {
     switch(pEvent->id)
     {
         case MQTT_EVENT_CONNECTED:
+            reconnectInterval = 3000;
             mqttStatus = MQTT_STATUS_CONNECTED;
             Trace(1,"MQTT connected, now subscribe topic:%s",SUBSCRIBE_TOPIC);
             MQTT_Error_t err;
@@ -219,6 +311,7 @@ void SecondTaskEventDispatch(MQTT_Event_t* pEvent)
             break;
         case MQTT_EVENT_DISCONNECTED:
             mqttStatus = MQTT_STATUS_DISCONNECTED;
+            StartTimerConnect(reconnectInterval,pEvent->client);
             break;
         default:
             break;
@@ -229,15 +322,18 @@ void SecondTask(void *pData)
 {
     MQTT_Event_t* event=NULL;
 
-    INFO_GetIMEI(imei);
-    Trace(1,"IMEI:%s",imei);
-
     semMqttStart = OS_CreateSemaphore(0);
     OS_WaitForSemaphore(semMqttStart,OS_WAIT_FOREVER);
     OS_DeleteSemaphore(semMqttStart);
+    semMqttStart = NULL;
+
     Trace(1,"start mqtt test");
+
+    INFO_GetIMEI(imei);
+    Trace(1,"IMEI:%s",imei);
+
     MQTT_Client_t* client = MQTT_ClientNew();
-    MQTT_Connect_Info_t ci;
+    
     MQTT_Error_t err;
     memset(&ci,0,sizeof(MQTT_Connect_Info_t));
     ci.client_id = imei;
--- demo/network/src/demo_network.c
+++ demo/network/src/demo_network.c
@@ -9,6 +9,11 @@
 #include <api_socket.h>
 
 
+#define PDP_CONTEXT_APN       "cmnet"
+#define PDP_CONTEXT_USERNAME  ""
+#define PDP_CONTEXT_PASSWD    ""
+
+
 #define DOMAIN_NUMBER 8
 
 #define MAIN_TASK_STACK_SIZE    (2048 * 2)
@@ -34,56 +39,51 @@ const char domains[DOMAIN_NUMBER][50] = {
 };
 
 
-void EventDispatch(API_Event_t* pEvent)
+bool AttachActivate()
+{
+    uint8_t status;
+    bool ret = Network_GetAttachStatus(&status);
+    if(!ret)
+    {
+        Trace(2,"get attach staus fail");
+        return false;
+    }
+    Trace(2,"attach status:%d",status);
+    if(!status)
+    {
+        ret = Network_StartAttach();
+        if(!ret)
+        {
+            Trace(2,"network attach fail");
+            return false;
+        }
+    }
+    else
+    {
+        ret = Network_GetActiveStatus(&status);
+        if(!ret)
+        {
+            Trace(2,"get activate staus fail");
+            return false;
+        }
+        Trace(2,"activate status:%d",status);
+        if(!status)
+        {
+            Network_PDP_Context_t context = {
+                .apn        = PDP_CONTEXT_APN,
+                .userName   = PDP_CONTEXT_USERNAME,
+                .userPasswd = PDP_CONTEXT_PASSWD
+            };
+            Network_StartActive(context);
+        }
+    }
+    return true;
+}
+
+static void NetworkEventDispatch(API_Event_t* pEvent)
 {
     switch(pEvent->id)
     {
-        case API_EVENT_ID_NO_SIMCARD:
-            Trace(10,"!!NO SIM CARD%d!!!!",pEvent->param1);
-            break;
-        
-        case API_EVENT_ID_SIMCARD_DROP:
-            Trace(2,"SIM CARD%d DROP",pEvent->param1);
-            break;
-        
-        case API_EVENT_ID_SIGNAL_QUALITY:
-            Trace(2,"signal quality:%d",pEvent->param1);
-            break;
-        
-
-        case API_EVENT_ID_NETWORK_REGISTERED_HOME:
-        case API_EVENT_ID_NETWORK_REGISTERED_ROAMING:
-        {
-            uint8_t status;
-            Trace(2,"network register success");
-            bool ret = Network_GetAttachStatus(&status);
-            if(!ret)
-                Trace(1,"get attach staus fail");
-            Trace(1,"attach status:%d",status);
-            if(status == 0)
-            {
-                ret = Network_StartAttach();
-                if(!ret)
-                {
-                    Trace(1,"network attach fail");
-                }
-            }
-            else
-            {
-                Network_PDP_Context_t context = {
-                    .apn        ="cmnet",
-                    .userName   = ""    ,
-                    .userPasswd = ""
-                };
-                Network_StartActive(context);
-            }
-
-            break;
-        }
-        case API_EVENT_ID_NETWORK_REGISTER_SEARCHING:
-            Trace(2,"network register searching");
-            break;
-        
         case API_EVENT_ID_NETWORK_REGISTER_DENIED:
             Trace(2,"network register denied");
             break;
@@ -92,38 +92,74 @@ void EventDispatch(API_Event_t* pEvent)
             Trace(2,"network register no");
             break;
         
+        case API_EVENT_ID_NETWORK_REGISTERED_HOME:
+        case API_EVENT_ID_NETWORK_REGISTERED_ROAMING:
+            Trace(2,"network register success");
+            AttachActivate();
+            break;
+
         case API_EVENT_ID_NETWORK_DETACHED:
             Trace(2,"network detached");
+            AttachActivate();
             break;
-        
         case API_EVENT_ID_NETWORK_ATTACH_FAILED:
             Trace(2,"network attach failed");
+            AttachActivate();
             break;
 
         case API_EVENT_ID_NETWORK_ATTACHED:
             Trace(2,"network attach success");
-            Network_PDP_Context_t context = {
-                .apn        ="cmnet",
-                .userName   = ""    ,
-                .userPasswd = ""
-            };
-            Network_StartActive(context);
+            AttachActivate();
             break;
 
         case API_EVENT_ID_NETWORK_DEACTIVED:
             Trace(2,"network deactived");
-            break;
-        
-        case API_EVENT_ID_NETWORK_ACTIVATE_FAILED:
-            Trace(2,"network activate failed");
-            break;
-        
-        case API_EVENT_ID_NETWORK_ACTIVATED:
-            Trace(2,"network activate success");
+            AttachActivate();
             break;
 
-        case API_EVENT_ID_NETWORK_GOT_TIME:
-            Trace(2,"network got time");
+        case API_EVENT_ID_NETWORK_ACTIVATE_FAILED:
+            Trace(2,"network activate failed");
+            AttachActivate();
+            break;
+
+        case API_EVENT_ID_NETWORK_ACTIVATED:
+            Trace(2,"network activate success..");
+            break;
+
+        case API_EVENT_ID_SIGNAL_QUALITY:
+            Trace(2,"CSQ:%d",pEvent->param1);
+            break;
+
+        default:
+            break;
+    }
+}
+
+static void EventDispatch(API_Event_t* pEvent)
+{
+    switch(pEvent->id)
+    {    
+        case API_EVENT_ID_NO_SIMCARD:
+            Trace(2,"!!NO SIM CARD%d!!!!",pEvent->param1);
+            break;
+        case API_EVENT_ID_SIMCARD_DROP:
+            Trace(2,"!!SIM CARD%d DROP!!!!",pEvent->param1);
+            break;
+        case API_EVENT_ID_SYSTEM_READY:
+            Trace(2,"system initialize complete");
+            break;
+        case API_EVENT_ID_NETWORK_REGISTER_DENIED:
+        case API_EVENT_ID_NETWORK_REGISTER_NO:
+        case API_EVENT_ID_NETWORK_REGISTERED_HOME:
+        case API_EVENT_ID_NETWORK_REGISTERED_ROAMING:
+        case API_EVENT_ID_NETWORK_DETACHED:
+        case API_EVENT_ID_NETWORK_ATTACH_FAILED:
+        case API_EVENT_ID_NETWORK_ATTACHED:
+        case API_EVENT_ID_NETWORK_DEACTIVED:
+        case API_EVENT_ID_NETWORK_ACTIVATE_FAILED:
+        case API_EVENT_ID_NETWORK_ACTIVATED:
+        case API_EVENT_ID_SIGNAL_QUALITY:
+            NetworkEventDispatch(pEvent);
             break;
 
         default:
@@ -135,17 +171,20 @@ void Test_MainTask(void* param)
 {
     uint8_t buffer[16];
     int i = 0;
+    
+    OS_Sleep(5000);
+
     while(1)
     {
         memset(buffer,0,sizeof(buffer));
         int ret = DNS_GetHostByName2(domains[i],buffer);
         if(ret != 0)
         {
-            Trace(2,"DNS get ip fail");
+            Trace(1,"DNS get ip fail");
         }
         else
         {
-            Trace(2,"DNS %s get ip success:%s",domains[i],buffer);
+            Trace(1,"DNS %s get ip success:%s",domains[i],buffer);
         }
         ++i;
         if(i>=DOMAIN_NUMBER)
